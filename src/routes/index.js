250637f9de58bd3a7a03adc13b11bb5206ef99e3
Paulo Galarza <paulo-123@hotmail.es>
2026-05-06 10:44:08 -0500

    feat: Diseno del formulario de registro y registro de su ruta

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,4 +8,8 @@ router.get("/", (req, res) => {
   res.sendFile(views + "/index.html");
 });
 
+router.get("/register", (req, res) => {
+  res.sendFile(views + "/register.html");
+});
+
 module.exports = router;
