977641de8ee59e7c4de3fd5bbe2bf86d2ac7526e
Paulo Galarza <paulo-123@hotmail.es>
2026-05-07 21:25:31 -0500

    feat: Configuracion cookie y diseno de vista y registro de ruta de registro de usuario

---
+++ src/public/js/register.js
@@ -0,0 +1,10 @@
+const login = document.querySelector("#login");
+login.addEventListener("click", () => {
+  const user = document.querySelector("#username").value;
+  if (user != "") {
+    document.cookie = `username=${user}`;
+    document.location.href = "/";
+  } else {
+    alert("Please enter a username");
+  }
+});
