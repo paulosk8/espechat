c1f3c462c069b20331209b2f9dfaed4cf62c89c9
Paulo Galarza <paulo-123@hotmail.es>
2026-05-07 21:41:20 -0500

    feat: Middleware para verificacion de usuario logeado

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ const express = require("express");
 const { createServer } = require("http");
 const realTimeServer = require("./realTimeServer");
 const path = require("path");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 const httpServer = createServer(app);
 
 app.set("port", process.env.PORT || 3000);
 app.set("views", path.join(__dirname, "views"));
+app.use(cookieParser());
 
 app.use(require("./routes"));
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const path = require("path");
-
+const isLoggedIn = require("../middleware/isLoggedIn");
 const views = path.join(__dirname, "/../views");
 
-router.get("/", (req, res) => {
+router.get("/", isLoggedIn, (req, res) => {
   res.sendFile(views + "/index.html");
 });
 
